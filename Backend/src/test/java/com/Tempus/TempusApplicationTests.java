3c4edb3940ac7421ff4ceff859f8194d2e26f894
Tomas Mendoza <tm1453766@gmail.com>
2025-02-24 11:37:08 -0300

    First commit

---
+++ Backend/src/test/java/com/Tempus/TempusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tempus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
