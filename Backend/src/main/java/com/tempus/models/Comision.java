542c1fcd21a6c21d0e8e4b85c002a1c33c7dc9b8
Tomas Mendoza <tm1453766@gmail.com>
2025-08-01 15:34:02 -0300

    feat: add atributo diasDeCursada a comision

--- Backend/src/main/java/com/tempus/models/Comision.java
+++ Backend/src/main/java/com/tempus/models/Comision.java
@@ -1,6 +1,7 @@
 package com.tempus.models;
 
 import com.tempus.Factory.strategy.IValidacionHorarioFactory;
+import com.tempus.enums.DayOfWeek;
 import com.tempus.enums.Turno;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -8,6 +9,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalTime;
+import java.util.Set;
 
 @Entity
 @Data
@@ -20,6 +22,10 @@ public class Comision {
     private Long idComision;
 
     private LocalTime horario;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Enumerated(EnumType.STRING)
+    private Set<DayOfWeek> diasDeCursada;
 
     @ManyToOne
     @JoinColumn(name = "materia_id")
