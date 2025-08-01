b5e8c64533de8f7e647a13d07b5b275d611be02e
Tomas Ismael Mendoza <100981517+tomasmendoza-unq@users.noreply.github.com>
2025-08-01 15:37:34 -0300

    Feat comision time (#7)
    * feat: add strategys para validacion de bandas horarias
    
    * feat: add  feature para obtener la estrategia de horario para las comisiones
    
    * feat: add feature para obtener comisiones por banda horaria
    
    * feat: add feature para determinar si se superponen horarios
    
    * feat: add atributo diasDeCursada a comision

---
+++ Backend/src/main/java/com/tempus/enums/DayOfWeek.java
@@ -0,0 +1,12 @@
+package com.tempus.enums;
+
+public enum DayOfWeek {
+
+    DOMINGO,
+    LUNES,
+    MARTES,
+    MIERCOLES,
+    JUEVES,
+    VIERNES,
+    SABADO
+}
--- Backend/src/main/java/com/tempus/models/Comision.java
+++ Backend/src/main/java/com/tempus/models/Comision.java
@@ -1,13 +1,14 @@
 package com.tempus.models;
 
-import com.tempus.Factory.strategy.IValidacionHorarioFactory;
-import com.tempus.enums.Turno;
+import com.tempus.enums.DayOfWeek;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalTime;
+import java.util.Set;
+
 
 @Entity
 @Data
@@ -20,6 +21,10 @@ public class Comision {
     private Long idComision;
 
     private LocalTime horario;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Enumerated(EnumType.STRING)
+    private Set<DayOfWeek> diasDeCursada;
 
     @ManyToOne
     @JoinColumn(name = "materia_id")
