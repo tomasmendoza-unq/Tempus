4cc13eaefac93e6517b96a4217610c5605569a44
Tomas Mendoza <tm1453766@gmail.com>
2025-12-10 00:18:26 -0300

    inicializacion repo

---
+++ src/test/java/edu/ar/tempus/TempusApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ar.tempus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
