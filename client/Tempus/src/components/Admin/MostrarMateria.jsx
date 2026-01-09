fec821c5f4716c37957d8d2b39c36fbeba887652
MunneLautaro <lautaro_munne@outlook.com>
2026-01-09 17:54:38 -0300

    Agrego los componentes principales que utilizan los services y un primer acercamiento de los nodos

---
+++ client/Tempus/src/components/Admin/MostrarMateria.jsx
@@ -0,0 +1,31 @@
+import { traerMateria } from "../../hooks/materia";
+import { useState } from "react";
+
+export default function MostrarMateria() {
+  const [materia, setMateria] = useState(null);
+  const [response, setResponse] = useState(null);
+
+  console.log(response);
+  return (
+    <form
+      className="flex bg-white min-h-25 min-w-25  items-center justify-center flex-col gap-3.5 p-6 rounded"
+      onSubmit={async (e) => {
+        e.preventDefault();
+        const data = await traerMateria(1);
+        setResponse(data);
+      }}
+    >
+      <input
+        onChange={(e) =>
+          setMateria((prev) => ({
+            ...prev,
+            nombre: e.target.value,
+          }))
+        }
+        value={materia ? materia.nombre : ""}
+        placeholder="idMateria..."
+      />
+      <button type="submit">Buscar Materia</button>
+    </form>
+  );
+}
