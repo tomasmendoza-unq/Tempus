ea8d8023df13fb174cf6b7f1d6718214df33d574
Tomas Mendoza <tm1453766@gmail.com>
2026-01-14 12:40:24 -0300

    feature: agrego validacion de que las comisiones no pueden tener el horario fin menor que el horario inicio

--- server/src/main/java/edu/ar/tempus/controller/ComisionControllerRest.java
+++ server/src/main/java/edu/ar/tempus/controller/ComisionControllerRest.java
@@ -4,6 +4,7 @@ import edu.ar.tempus.controller.dto.comision.ComisionDTORequest;
 import edu.ar.tempus.controller.dto.comision.ComisionDTOResponse;
 import edu.ar.tempus.model.Comision;
 import edu.ar.tempus.service.ComisionService;
+import jakarta.validation.Valid;
 import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -27,7 +28,7 @@ public final class ComisionControllerRest {
     }
 
     @PostMapping("/crear")
-    public ResponseEntity<ComisionDTOResponse> crearComision(@RequestBody ComisionDTORequest request){
+    public ResponseEntity<ComisionDTOResponse> crearComision(@Valid @RequestBody ComisionDTORequest request){
         Comision comision = ComisionDTORequest.aModelo(request);
 
         Comision comisionGuardada = comisionService.guardar(comision, request.materiaId());
--- server/src/main/java/edu/ar/tempus/controller/dto/comision/ComisionDTORequest.java
+++ server/src/main/java/edu/ar/tempus/controller/dto/comision/ComisionDTORequest.java
@@ -2,14 +2,28 @@ package edu.ar.tempus.controller.dto.comision;
 
 import edu.ar.tempus.model.Comision;
 import edu.ar.tempus.model.Materia;
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalTime;
 
 public record ComisionDTORequest(
+        @NotNull(message = "El horario de inicio es requerido")
         LocalTime horarioInicio,
+
+        @NotNull(message = "El horario de fin es requerido")
         LocalTime horarioFin,
+
+        @NotNull(message = "La materia es requerida")
         Long materiaId
 ) {
+
+
+    @AssertTrue(message = "El horario de fin debe ser posterior al horario de inicio")
+    private boolean isHorarioValido() {
+        return horarioFin.isAfter(horarioInicio);
+    }
+
     public static Comision aModelo(ComisionDTORequest request) {
         return Comision.builder()
                 .horarioInicio(request.horarioInicio)
