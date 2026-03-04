8ac6e73c694baa548584bdf92964bc2f990201ff
Tomas Mendoza <tm1453766@gmail.com>
2026-03-03 21:41:20 -0300

    refactor: cambio el high del grid para que se vean las materias seleccionadas

--- client/Tempus/src/pages/Carreras.jsx
+++ client/Tempus/src/pages/Carreras.jsx
@@ -9,8 +9,8 @@ export default function Carreras() {
         Gestión de Carreras
       </h1>
 
-      <div className="flex-1 flex flex-col gap-6 max-w-7xl mx-auto w-full">
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 h-[500px]">
+      <div className="flex flex-col gap-6 max-w-7xl mx-auto w-full">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 h-fit">
           <div className="h-full">
             <CarreraForm />
           </div>
