019154d4ca7d4cf012016302641b49012de4668e
Tomas Mendoza <tm1453766@gmail.com>
2026-03-10 16:44:44 -0300

    feature: el titulo de tempus en la navbar ahora es un link para ir a home

--- client/Tempus/src/components/Ui/NavBar/NavBar.jsx
+++ client/Tempus/src/components/Ui/NavBar/NavBar.jsx
@@ -8,11 +8,27 @@ export default function NavBar() {
 
   return (
     <header className="flex items-center justify-between px-8 py-4 shadow bg-red-950 text-white">
+      
       <div className="flex items-center gap-8">
-        <h1 className="text-2xl font-bold">Tempus</h1>
-        
+
+        <Link
+          to="/"
+          className="
+            relative inline-block
+            after:absolute after:left-0 after:bottom-[-2px]
+            after:h-[2px] after:w-full
+            after:bg-red-400
+            after:origin-left after:scale-x-0
+            after:transition-transform after:duration-300
+            hover:after:scale-x-100
+          "
+        >
+          <h1 className="text-2xl font-bold">Tempus</h1>
+        </Link>
+
         {isAuthenticated && <CarreraSelector />}
       </div>
+
       <HamburgerMenu
         isAuthenticated={isAuthenticated}
         usuario={usuario}
