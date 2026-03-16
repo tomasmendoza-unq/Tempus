64b770191a7c1d0d5117abc1b37554718553ecdc
Tomas Mendoza <tm1453766@gmail.com>
2026-03-16 19:19:21 -0300

    refactor: elimino la dependencia de vincular las comisiones cuando se genera una comision

--- server/src/main/java/edu/ar/tempus/persistence/neo4J/ComisionNeo4JDAO.java
+++ server/src/main/java/edu/ar/tempus/persistence/neo4J/ComisionNeo4JDAO.java
@@ -10,38 +10,44 @@ import java.util.List;
 
 @Repository
 public interface ComisionNeo4JDAO extends Neo4jRepository<ComisionNeo4J, Long> {
-    @Query("""
-            MATCH (c1:Comision {id: $id}), (c2:Comision)
-            WHERE c1.id <> c2.id
-              AND NOT EXISTS {
-                MATCH (c1)-[:SE_DICTA_EL]->(h1:ClaseHorario),
-                      (c2)-[:SE_DICTA_EL]->(h2:ClaseHorario)
-                WHERE h1.dia = h2.dia
-                  AND h1.inicio < h2.fin
-                  AND h1.fin > h2.inicio
-              }
-              AND EXISTS { MATCH (c1)-[:SE_DICTA_EL]->() }
-              AND EXISTS { MATCH (c2)-[:SE_DICTA_EL]->() }
-            MERGE (c1)-[:COMPATIBLE_CON]->(c2)
-            """)
-    void vincularCompatibilidadesPorId(@Param("id") Long id);
 
-    @Query("MATCH (c:Comision {id: $idA})-[:COMPATIBLE_CON]-(target:Comision {id: $idB}) RETURN count(target) > 0")
+    @Query("""
+    MATCH (c1:Comision {id: $idA})
+    MATCH (c2:Comision {id: $idB})
+    RETURN NOT EXISTS {
+        MATCH (c1)-[:SE_DICTA_EL]->(h1:ClaseHorario),
+              (c2)-[:SE_DICTA_EL]->(h2:ClaseHorario)
+        WHERE h1.dia = h2.dia
+          AND h1.inicio < h2.fin
+          AND h1.fin > h2.inicio
+    }
+    """)
     boolean verificarCompatibilidad(Long idA, Long idB);
 
     @Query("""
     MATCH (c1:Comision) WHERE c1.id IN $nuevas
     WITH collect(c1) AS nuevas
-    MATCH (c2:Comision) WHERE c2.id IN $nuevas + $anotadas
+    
+    MATCH (c2:Comision) WHERE c2.id IN ($nuevas + $anotadas)
     WITH nuevas, collect(c2) AS todas
+    
     UNWIND nuevas AS c1
     UNWIND todas AS c2
+    
     WITH c1, c2
-    WHERE c1.id <> c2.id AND NOT (c1)-[:COMPATIBLE_CON]-(c2)
+    WHERE c1.id <> c2.id
+    AND EXISTS {
+        MATCH (c1)-[:SE_DICTA_EL]->(h1:ClaseHorario),
+              (c2)-[:SE_DICTA_EL]->(h2:ClaseHorario)
+        WHERE h1.dia = h2.dia
+          AND h1.inicio < h2.fin
+          AND h1.fin > h2.inicio
+    }
+    
     RETURN COUNT(*) > 0
     """)
     boolean haySuperposicionHoraria(
-            @Param("nuevas") List<Long> comisionIds,
-            @Param("anotadas") List<Long> comisionesAnotadas
+            @Param("nuevas") List<Long> nuevas,
+            @Param("anotadas") List<Long> anotadas
     );
 }
