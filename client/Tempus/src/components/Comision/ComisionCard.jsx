658f27fc8567d799efc50e678adfed47c98eb6db
MunneLautaro <lautaro_munne@outlook.com>
2026-03-17 17:47:40 -0300

    Se modifica Comision card para recibir children y que sea mas reutilizable

--- client/Tempus/src/components/Comision/ComisionCard.jsx
+++ client/Tempus/src/components/Comision/ComisionCard.jsx
@@ -1,11 +1,6 @@
-import { Trash2, Edit } from "feather-icons-react"
-import { useNavigate } from "react-router-dom"
-
-export default function ComisionCard({ comision }) {
-  const navigate = useNavigate()
-
+export default function ComisionCard({ comision, children }) {
   return (
-    <div className="w-full max-w-xs flex flex-col justify-center items-center border-2 border-gray-950 bg-white py-5 text-black hover:shadow-md hover:shadow-red-700 hover:bg-red-300 duration-200 rounded rounded-2xl">
+    <div className="w-full max-w-xs flex flex-col justify-center items-center border-2 hover:scale-110 border-gray-950 bg-white py-5 text-black hover:shadow-md hover:shadow-red-700 hover:bg-red-300 duration-200 rounded-2xl">
       <h2 className="text-xl font-bold mb-2 text-red-500">
         Comision: #{comision?.comisionId || "Sin nombre"}
       </h2>
@@ -21,22 +16,7 @@ export default function ComisionCard({ comision }) {
       ) : (
         <p className="text-md">Sin horarios</p>
       )}
-      <div>
-        <button
-          onClick={() =>
-            navigate(`/comisiones/eliminar/${comision?.comisionId}`)
-          }
-          className="mt-4 text-red-600 hover:bg-red-900 transition-colors duration-200 hover:text-white font-bold py-2 px-4 rounded"
-        >
-          <Trash2 />
-        </button>
-        <button
-          onClick={() => navigate(`/comisiones/editar/${comision?.comisionId}`)}
-          className="mt-4 text-green-600 hover:bg-green-900 transition-colors duration-200 hover:text-white font-bold py-2 px-4 rounded"
-        >
-          <Edit />
-        </button>
-      </div>
+      {children}
     </div>
   )
 }
