031e0c087a05fb7f9630f1b2fac2a5fcc8b8560e
MunneLautaro <lautaro_munne@outlook.com>
2026-03-17 10:08:14 -0300

    Se cambian estilos de la tarjeta y display de comisiones y se usan los datos de la BD

--- client/Tempus/src/components/Comision/ComisionCard.jsx
+++ client/Tempus/src/components/Comision/ComisionCard.jsx
@@ -5,14 +5,22 @@ export default function ComisionCard({ comision }) {
   const navigate = useNavigate()
 
   return (
-    <div className="flex flex-col border border-gray-500 bg-white p-5 text-black hover:shadow-md hover:shadow-red-700 hover:bg-red-300 duration-200 rounded">
-      <h2 className="text-xl font-bold mb-2">
-        {comision?.comisionId || "Sin nombre"}
+    <div className="w-full max-w-xs flex flex-col justify-center items-center border-2 border-gray-950 bg-white py-5 text-black hover:shadow-md hover:shadow-red-700 hover:bg-red-300 duration-200 rounded rounded-2xl">
+      <h2 className="text-xl font-bold mb-2 text-red-500">
+        Comision: #{comision?.comisionId || "Sin nombre"}
       </h2>
       <p className="text-lg mb-2">
         {comision?.materia?.materiaNombre || "Sin nombre"}
       </p>
-      <p className="text-md">{comision?.claseHorario || "Sin nombre"}</p>
+      {comision?.claseHorario?.length > 0 ? (
+        comision.claseHorario.map((h, i) => (
+          <p key={i} className="text-md">
+            {h.dia} · {h.inicio} - {h.fin}
+          </p>
+        ))
+      ) : (
+        <p className="text-md">Sin horarios</p>
+      )}
       <div>
         <button
           onClick={() =>
